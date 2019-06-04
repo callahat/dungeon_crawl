7e4c5a11929431ea23554b5b11530ec41374cc97
callahat <tim.callahan25@yahoo.com>
2019-06-03 20:52:36 -0400

    Adding schema dump moved map_tile update and basic tile template seeds to the migration.

--- priv/repo/migrations/20190527233310_create_tile_templates.exs
+++ priv/repo/migrations/20190527233310_create_tile_templates.exs
@@ -1,7 +1,10 @@
 defmodule DungeonCrawl.Repo.Migrations.CreateTileTemplates do
   use Ecto.Migration
 
-  def change do
+  alias DungeonCrawl.Repo
+  alias DungeonCrawl.Dungeon.MapTile
+
+  def up do
     create table(:tile_templates) do
       add :name, :string
       add :character, :string
@@ -16,5 +19,33 @@ defmodule DungeonCrawl.Repo.Migrations.CreateTileTemplates do
     alter table(:dungeon_map_tiles) do
       add :tile_template_id, references(:tile_templates, on_delete: :nothing)
     end
+
+    flush()
+
+    template_map = DungeonCrawl.TileTemplates.TileSeeder.basic_tiles
+
+    # Link all the map tiles that aren't already linked
+    counts = Repo.all(MapTile)
+             |> Enum.reduce(%{},fn(mt,acc) -> if template_map[mt.tile] do
+                        Repo.update! MapTile.changeset(mt, %{tile_template_id: template_map[mt.tile].id})
+                        Map.put(acc, mt.tile, if(acc[mt.tile], do: acc[mt.tile]+1, else: 1))
+                      else
+                        Map.put(acc, mt.tile, if(acc[mt.tile], do: acc[mt.tile]+1, else: 1))
+                      end
+             end)
+    counts
+    |> inspect
+    |> IO.puts
+
+    IO.puts "Characters that didn't exist in the template map (if any, add them and rereun seeds)"
+    IO.puts inspect Map.keys(counts) -- Map.keys(template_map)
+  end
+
+  def down do
+    alter table(:dungeon_map_tiles) do
+      drop :tile_template_id
+    end
+
+    drop table(:tile_templates)
   end
 end
