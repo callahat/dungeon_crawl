d8ca6ad2bf1f55c9663f62830f28812c5fe54d1f
callahat <tim.callahan25@yahoo.com>
2019-06-26 07:19:59 -0400

    UI around joining instances/creating new instance

--- lib/dungeon_crawl_web/controllers/dungeon_controller.ex
+++ lib/dungeon_crawl_web/controllers/dungeon_controller.ex
@@ -2,14 +2,15 @@ defmodule DungeonCrawlWeb.DungeonController do
   use DungeonCrawl.Web, :controller
 
   alias DungeonCrawl.Dungeon
+  alias DungeonCrawl.DungeonInstances
   alias DungeonCrawl.Dungeon.Map
   alias DungeonCrawl.DungeonGenerator
 
   @dungeon_generator Application.get_env(:dungeon_crawl, :generator) || DungeonGenerator
 
   def index(conn, _params) do
-    dungeons_and_counts = Dungeon.list_dungeons_with_player_count()
-    render(conn, "index.html", dungeons_and_counts: dungeons_and_counts)
+    dungeons = Dungeon.list_dungeons_with_player_count()
+    render(conn, "index.html", dungeons: dungeons)
   end
 
   def new(conn, _params) do
@@ -28,17 +29,17 @@ defmodule DungeonCrawlWeb.DungeonController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    dungeon = Dungeon.get_map!(id) |> Repo.preload(dungeon_map_tiles: :tile_template)
-    dungeon_render = 
-      dungeon.dungeon_map_tiles
-      |> Enum.sort(fn(a,b) -> {a.row, a.col} < {b.row, b.col} end)
-      |> Enum.map(fn(row) -> row.tile_template.character end)
-      |> to_charlist
-      |> Enum.chunk(dungeon.width)
-      |> Enum.join("\n")
+  def show(conn, %{"id" => id, "instance_id" => instance_id}) do
+    dungeon = Dungeon.get_map!(id) |> Repo.preload([map_instances: [:locations]])
+    instance = DungeonInstances.get_map!(instance_id) |> Repo.preload([dungeon_map_tiles: [:tile_template]])
 
-    render(conn, "show.html", dungeon: dungeon, dungeon_render: dungeon_render)
+    render(conn, "show.html", dungeon: dungeon, instance: instance)
+  end
+
+  def show(conn, %{"id" => id}) do
+    dungeon = Dungeon.get_map!(id) |> Repo.preload([map_instances: [:locations], dungeon_map_tiles: [:tile_template]])
+
+    render(conn, "show.html", dungeon: dungeon, instance: nil)
   end
 
   def delete(conn, %{"id" => id}) do
