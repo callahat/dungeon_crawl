56b370ed09c87341b065297e8de81e88a02b2220
callahat <tim.callahan25@yahoo.com>
2019-09-19 21:24:22 -0400

    DRY up the player actions (other than move, since thats still a special case) in dungeon_channel

--- lib/dungeon_crawl_web/channels/dungeon_channel.ex
+++ lib/dungeon_crawl_web/channels/dungeon_channel.ex
@@ -45,37 +45,30 @@ defmodule DungeonCrawlWeb.DungeonChannel do
   end
 
   def handle_in("use_door", %{"direction" => direction, "action" => action}, socket) when action == "OPEN" or action == "CLOSE" do
-    player_location = Player.get_location!(socket.assigns.user_id_hash) |> Repo.preload(:map_tile)
-    target_door = Dungeon.get_map_tile(player_location.map_tile, direction) |> Repo.preload(:tile_template)
-
-    # TODO: eventually grab the running program and have it try the label instead of this.
-    # Once its more of a step (instead of run everything til a halt condition) it might be better to have the runner return
-    # information such as events to broadcast (instead of broadcasting the event itself)
-    script = target_door.script
-    {:ok, prog} = DungeonCrawl.Scripting.Parser.parse script
-
-    if prog.labels[action] do
-      %{program: prog, object: target_door} = DungeonCrawl.Scripting.Runner.run %{program: prog, object: target_door, label: action}
-      _handle_broadcasts(socket, prog.broadcasts)
-      {:reply, _reply_payload(prog.responses), socket}
-    else
-      {:reply, {:error, %{msg: "Cannot #{String.downcase(action)} that"}}, socket}
-    end
+    _player_action_helper(
+      %{"direction" => direction, "action" => action},
+      {:reply, {:error, %{msg: "Cannot #{String.downcase(action)} that"}}, socket},
+      socket)
   end
 
   def handle_in("step", %{"direction" => direction}, socket) do
+    _player_action_helper(%{"direction" => direction, "action" => "TOUCH"}, {:noreply, socket}, socket)
+  end
+
+  defp _player_action_helper(%{"direction" => direction, "action" => action}, no_label_response, socket) do
     player_location = Player.get_location!(socket.assigns.user_id_hash) |> Repo.preload(:map_tile)
     target_tile = Dungeon.get_map_tile(player_location.map_tile, direction) |> Repo.preload(:tile_template)
 
+    # TODO: eventually grab the running program and have it try the label instead of this.
     script = target_tile.script
     {:ok, prog} = DungeonCrawl.Scripting.Parser.parse script
 
-    if prog.labels["TOUCH"] do
-      %{program: prog, object: target_tile} = DungeonCrawl.Scripting.Runner.run %{program: prog, object: target_tile, label: "TOUCH"}
+    if prog.labels[action] do
+      %{program: prog, object: target_tile} = DungeonCrawl.Scripting.Runner.run %{program: prog, object: target_tile, label: action}
       _handle_broadcasts(socket, prog.broadcasts)
       {:reply, _reply_payload(prog.responses), socket}
     else
-      {:noreply, socket}
+      no_label_response
     end
   end
 
