6aa1832b92e8d067966c75a2438f4a4686f5d0de
callahat <tim.callahan25@yahoo.com>
2020-01-21 23:44:21 -0500

    Adding sidebar/main layout hook for the space for the sidebar if needed. Adding sidebar for dungeon editor, using bootstrap tab and removing custom JS to show/hide the different tool pallettes/expands.

--- lib/dungeon_crawl_web/views/layout_view.ex
+++ lib/dungeon_crawl_web/views/layout_view.ex
@@ -1,3 +1,11 @@
 defmodule DungeonCrawlWeb.LayoutView do
   use DungeonCrawl.Web, :view
+
+  def main_tag_class(assigns) do
+    if Map.get(assigns, :sidebar_present_md) do
+      "ml-sm-auto col-md-9 col-lg-9 px-4"
+    else
+      "ml-sm-auto col-md-12 col-lg-12 px-4"
+    end
+  end
 end
