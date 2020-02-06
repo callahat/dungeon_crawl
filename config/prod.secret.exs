693a92425b67ce23dab1a37a6d6a9f5b4d79f71c
callahat <tim.callahan25@yahoo.com>
2020-02-05 19:48:32 -0500

    Use production config from Heroku ENV variables and decrease socket timeout
    Commenting otu some old data migrations as I cant ecto.load the schema dump in heroku

---
+++ config/prod.secret.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :dungeon_crawl, DungeonCrawl.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :dungeon_crawl, DungeonCrawl.Repo,
+  ssl: true,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DATABASE_USERNAME"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  database: "dungeon_crawl_prod",
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
--- priv/repo/migrations/20190612023436_add_z_index_to_map_tiles.exs
+++ priv/repo/migrations/20190612023436_add_z_index_to_map_tiles.exs
@@ -12,7 +12,7 @@ defmodule DungeonCrawl.Repo.Migrations.AddZIndexToMapTiles do
     flush()
 
     # Update with z_index 0
-    Repo.all(MapTile)
-    |> Enum.each(fn(mt) -> Dungeon.update_map_tile!(mt, %{z_index: 0}) end)
+    # Repo.all(MapTile)
+    # |> Enum.each(fn(mt) -> Dungeon.update_map_tile!(mt, %{z_index: 0}) end)
   end
 end
