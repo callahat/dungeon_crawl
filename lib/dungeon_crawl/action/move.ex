1b390732aa24b707dadb006fc24d200cfdc446e3
callahat <tim.callahan25@yahoo.com>
2020-03-24 22:59:54 -0400

    Refactored Move.go to use a cond instead of if

--- lib/dungeon_crawl/action/move.ex
+++ lib/dungeon_crawl/action/move.ex
@@ -1,19 +1,20 @@
 defmodule DungeonCrawl.Action.Move do
   alias DungeonCrawl.DungeonProcesses.Instances
   alias DungeonCrawl.DungeonInstances.MapTile
-  alias DungeonCrawl.TileState.Parser
 
   # todo: rename this
   def go(%MapTile{} = entity_map_tile, %MapTile{} = destination, %Instances{} = state) do
-    if can_move(destination) do
-      top_tile = Map.take(destination, [:map_instance_id, :row, :col, :z_index])
-      {new_location, state} = Instances.update_map_tile(state, entity_map_tile, Map.put(top_tile, :z_index, top_tile.z_index+1))
-      old_location_top_tile = Instances.get_map_tile(state, Map.take(entity_map_tile, [:row, :col]))
-      old_location = if old_location_top_tile, do: old_location_top_tile, else: Map.merge(%MapTile{}, Map.take(entity_map_tile, [:row, :col]))
-      {:ok, %{new_location: new_location, old_location: old_location}, state}
-    else
-      # might change this later to have more info, ie, bumped wall, cant pass through solid rock, etc
-      {:invalid}
+    cond do
+      destination.parsed_state[:blocking] ->
+        {:invalid}
+
+      true ->
+        top_tile = Map.take(destination, [:map_instance_id, :row, :col, :z_index])
+        {new_location, state} = Instances.update_map_tile(state, entity_map_tile, Map.put(top_tile, :z_index, top_tile.z_index+1))
+        old_location_top_tile = Instances.get_map_tile(state, Map.take(entity_map_tile, [:row, :col]))
+        old_location = if old_location_top_tile, do: old_location_top_tile, else: Map.merge(%MapTile{}, Map.take(entity_map_tile, [:row, :col]))
+        {:ok, %{new_location: new_location, old_location: old_location}, state}
+
     end
   end
   def go(_, _, _), do: {:invalid}
