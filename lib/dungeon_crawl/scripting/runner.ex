76fe53e66037fbe035a79ccb9f0068e6a20dc96a
callahat <tim.callahan25@yahoo.com>
2020-06-01 22:05:47 -0400

    guard against a tile that has removed itself but still has a running program in the context.

--- lib/dungeon_crawl/scripting/runner.ex
+++ lib/dungeon_crawl/scripting/runner.ex
@@ -27,9 +27,14 @@ require Logger
     end
   end
 
-  def run(%Runner{program: program} = runner_state) do
+  def run(%Runner{program: program, object_id: object_id, state: state} = runner_state) do
     if program.message == {} do
-      _run(runner_state)
+      # todo: maybe have the check for active tile live elsewhere
+      if Instances.get_map_tile_by_id(state, %{id: object_id}) do
+        _run(runner_state)
+      else
+        runner_state
+      end
     else
       {label, sender} = program.message
       run(%{ runner_state | event_sender: sender, program: %{ program | message: {} } }, label)
