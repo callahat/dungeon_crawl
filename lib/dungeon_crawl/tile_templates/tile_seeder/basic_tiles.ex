307055537359cc17cda538d3d01064d2124ea33f
callahat <tim.callahan25@yahoo.com>
2020-08-24 23:16:56 -0400

    player tiles should be "soft"

--- lib/dungeon_crawl/tile_templates/tile_seeder/basic_tiles.ex
+++ lib/dungeon_crawl/tile_templates/tile_seeder/basic_tiles.ex
@@ -100,7 +100,7 @@ defmodule DungeonCrawl.TileTemplates.TileSeeder.BasicTiles do
       %{character: "@",
         name: "Player",
         description: "Its a player.",
-        state: "blocking: true, pushable: true, health: 100, gems: 0, cash: 0, ammo: 6, bullet_damage: 10, player: true"}
+        state: "blocking: true, soft: true, pushable: true, health: 100, gems: 0, cash: 0, ammo: 6, bullet_damage: 10, player: true"}
     )
   end
 
