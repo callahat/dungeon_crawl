1acd3611ee15353b614a32ca0cf69cf03ff8c3ce
callahat <tim.callahan25@yahoo.com>
2021-04-25 18:23:22 -0400

    was complaing about that

--- priv/repo/migrations/20210423205519_change_scores_duration_from_timestamp_to_integer.exs
+++ priv/repo/migrations/20210423205519_change_scores_duration_from_timestamp_to_integer.exs
@@ -1,10 +1,17 @@
 defmodule DungeonCrawl.Repo.Migrations.ChangeScoresDurationFromTimestampToInteger do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:scores) do
       remove :duration
       add :duration, :integer
+    end
+  end
+
+  def down do
+    alter table(:scores) do
+      remove :duration
+      add :duration, :time
     end
   end
 end
