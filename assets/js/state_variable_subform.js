c4157ef5c502fe60c47b2bbb6e830034d57c9b12
callahat <tim.callahan25@yahoo.com>
2021-06-22 08:40:24 -0400

    WIP - need to pass in the standard variables to the form from each context (dungeon, level, tile, tile template) it is referenced Added js support and a dropdown button to quickly add standard variables. If a standard variable already exists among the form list, then that item will be disabled in the dropdown button.

--- assets/js/state_variable_subform.js
+++ assets/js/state_variable_subform.js
@@ -1,9 +1,17 @@
 let StateVariableSubform = {
   init(element){ if(!element){ return }
-    let field_name_prefix = element.getAttribute("data-field-name-prefix")
+    let fieldNamePrefix = element.getAttribute("data-field-name-prefix"),
+        standardVariableDropdownList = document.getElementById(fieldNamePrefix + "_standard_variable_dropdown_list")
 
-    document.getElementById(field_name_prefix + "_add_state_field_button").addEventListener('click', e => {
+    document.getElementById(fieldNamePrefix + "_add_state_field_button").addEventListener('click', e => {
       this.createNewRow(element);
+    })
+    document.getElementById(fieldNamePrefix + "_standard_variable_dropdown_button").addEventListener('click', e => {
+      // update to enable/disable the items based on the existing state values in the field
+      this.updateStandardVariables(element, standardVariableDropdownList)
+    })
+    standardVariableDropdownList.addEventListener('click', e => {
+      this.addStandardVariable(element, e.target)
     })
     element.addEventListener('click', e => {
       this.deleteRow(e.target);
@@ -13,7 +21,7 @@ let StateVariableSubform = {
     })
   },
   generateInitialRows(element, state_string) { if(!element){ return }
-    let field_name_prefix = element.getAttribute("data-field-name-prefix")
+    let fieldNamePrefix = element.getAttribute("data-field-name-prefix")
 
     $(".tile_template_state_data_row").remove()
 
@@ -29,15 +37,36 @@ let StateVariableSubform = {
   },
   createNewRow(element, variable = "", value = "") {
     let twoFields = document.createElement("tr"),
-        field_name_prefix = element.getAttribute("data-field-name-prefix")
+        fieldNamePrefix = element.getAttribute("data-field-name-prefix")
 
-    twoFields.classList.add(field_name_prefix + "_state_data_row")
+    twoFields.classList.add(fieldNamePrefix + "_state_data_row")
     twoFields.innerHTML = `
-      <td><input class="form-control" name="${field_name_prefix}[state_variables][]" type="text" value="${variable}"></td>
-      <td><input class="form-control" name="${field_name_prefix}[state_values][]" type="text" value="${value}"></td>
+      <td><input class="form-control" name="${fieldNamePrefix}[state_variables][]" type="text" value="${variable}"></td>
+      <td><input class="form-control" name="${fieldNamePrefix}[state_values][]" type="text" value="${value}"></td>
       <td><button type="button" class="btn btn-danger delete-state-fields-row">X</button></td>
     `
-    element.insertBefore(twoFields, document.getElementById(field_name_prefix + "_add_state_field_row"))
+    element.insertBefore(twoFields, document.getElementById(fieldNamePrefix + "_add_state_field_row"))
+  },
+  updateStandardVariables(element, standardVariableDropdownListElement){
+    let existingRows = element.getElementsByClassName("dungeon_state_data_row"),
+        existingVariables = Array.from(existingRows).map(e => {return e.getElementsByClassName("form-control")[0].value})
+
+    for(let standardVariable of standardVariableDropdownListElement.getElementsByClassName("dropdown-item")){
+      if(existingVariables.includes(standardVariable.innerText)){
+        standardVariable.classList.add("disabled")
+      } else {
+        standardVariable.classList.remove("disabled")
+      }
+    }
+  },
+  addStandardVariable(element, standardListTarget){ if(standardListTarget.tagName != "A") { return }
+     let existingRows = element.getElementsByClassName("dungeon_state_data_row"),
+         lastRow = existingRows[existingRows.length - 1]
+     if(lastRow.getElementsByClassName("form-control")[0].value == ""){
+       lastRow.remove()
+     }
+     this.createNewRow(element, standardListTarget.innerText)
+     this.createNewRow(element)
   },
   deleteRow(target) { if(!target.matches('.delete-state-fields-row')) { return }
     target.parentElement.parentElement.remove()
