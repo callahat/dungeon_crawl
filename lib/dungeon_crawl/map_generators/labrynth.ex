c77bf758863284903db0257e61dc749a0a704dc3
callahat <tim.callahan25@yahoo.com>
2021-06-28 23:04:24 -0400

    Adding entities to the labrynth map generator. Modifying the functions to track each dead end and use those for entity/stairs placement.

--- lib/dungeon_crawl/map_generators/labrynth.ex
+++ lib/dungeon_crawl/map_generators/labrynth.ex
@@ -2,7 +2,7 @@ defmodule DungeonCrawl.MapGenerators.Labrynth do
   @cave_height     39
   @cave_width      79
 
-  #@entities        Enum.to_list(?A..?|)
+  alias DungeonCrawl.MapGenerators.Entities
 
   @doc """
   Generates a labrynth.
@@ -30,22 +30,33 @@ defmodule DungeonCrawl.MapGenerators.Labrynth do
     seed_queue = _add_to_seed_queue([], initial_seed)
 
 # DungeonCrawl.MapGenerators.Labrynth.generate(5,5) |> DungeonCrawl.MapGenerators.Utils.stringify(5) |> IO.puts
-    _dig_tunnels({Map.put(map, initial_seed, ?.), seed_queue, initial_seed})
-    |> _stairs_up(solo_level, cave_height, cave_width)
+    _dig_tunnels({Map.put(map, initial_seed, ?.), seed_queue, initial_seed, []})
+    |> _stairs_up(solo_level)
+    |> _add_entities(solo_level || [])
   end
 
-  defp _stairs_up(map, solo_level, cave_height, cave_width) when is_integer(solo_level) do
-    row = :rand.uniform(cave_height) - 1
-    col = :rand.uniform(cave_width) - 1
-
-    if map[{row, col}] != ?# && _adjacent_walls(map, row, col) == 3 do
-      Map.put(map, {row, col}, ?▟)
-    else
-      _stairs_up(map, solo_level, cave_height, cave_width)
-    end
+  defp _stairs_up({map, [stair_coords | dead_ends]}, solo_level) when is_integer(solo_level) do
+    {Map.put(map, stair_coords, ?▟), dead_ends}
   end
 
-  defp _stairs_up(map, _, _, _), do: map
+  defp _stairs_up({map, dead_ends}, _), do: {map, dead_ends}
+
+  defp _add_entities({map, dead_ends}, solo_level) when is_integer(solo_level) do
+    max_entities = Enum.min [round(solo_level / 4) + 3, length(dead_ends)]
+    min_entities = Enum.min [round(solo_level / 10) + 1, max_entities]
+    entities = Entities.randomize(_rand_range(min_entities, max_entities))
+    _add_entities({map, dead_ends}, entities)
+  end
+
+  defp _add_entities({map, _}, []), do: map
+
+  defp _add_entities({map, []}, _), do: map
+
+  defp _add_entities({map, [entity_coords | dead_ends]}, [entity | entities]) do
+    _add_entities({Map.put(map, entity_coords, entity), dead_ends}, entities)
+  end
+
+  defp _add_entities({map, _}, _), do: map
 
   defp _adjacent_walls(map, row, col) do
     [ map[{row+1, col}],
@@ -66,7 +77,7 @@ defmodule DungeonCrawl.MapGenerators.Labrynth do
     queue ++ [{row, col}]
   end
 
-  # if two  steps in each direction is either wall or outside coordinate range
+  # if two  steps in each direction is either not wall or outside coordinate range
   defp _seed_surrounded(map, {row, col}) do
     map[{row-2, col}] != ?# &&
     map[{row+2, col}] != ?# &&
@@ -74,9 +85,14 @@ defmodule DungeonCrawl.MapGenerators.Labrynth do
     map[{row, col+2}] != ?#
   end
 
+  defp _dead_end(map, {row, col}) do
+    _seed_surrounded(map, {row, col}) &&
+      _adjacent_walls(map, row, col) == 3
+  end
+
   # For each direction 75% chance to dig a tunnel two squares, each end becomes a new seed
-  defp _dig_tunnels({map, seed_queue, {row, col}}) do
-    {map, seed_queue, {row, col}}
+  defp _dig_tunnels({map, seed_queue, {row, col}, dead_ends}) do
+    {map, seed_queue, {row, col}, dead_ends}
     |> _maybe_dig({-2, 0})
     |> _maybe_dig({ 2, 0})
     |> _maybe_dig({ 0,-2})
@@ -87,35 +103,43 @@ defmodule DungeonCrawl.MapGenerators.Labrynth do
     |> _dig_tunnels()
   end
 
-  defp _dig_tunnels({map, [], []}) do
-    map
+  defp _dig_tunnels({map, [], [], dead_ends}) do
+    {map, Enum.shuffle(Enum.uniq(dead_ends))}
   end
 
-  defp _maybe_dig({map, seed_queue, {row, col}}, {d_row, d_col}) do
+  defp _maybe_dig({map, seed_queue, {row, col}, dead_ends}, {d_row, d_col}) do
     if map[{row + d_row, col + d_col}] == ?# && :rand.uniform(4) == 1 do
       {
         Map.put(map, {row + round(d_row/2), col + round(d_col/2)}, ?.) |> Map.put({row + d_row, col + d_col}, ?.),
         _add_to_seed_queue(seed_queue, {row + d_row, col + d_col}),
-        {row, col}
+        {row, col},
+        dead_ends
       }
     else
-      {map, seed_queue, {row, col}}
+      {map, seed_queue, {row, col}, dead_ends}
     end
   end
 
-  defp _add_back_to_seed_queue({map, seed_queue, last_seed}) do
-    {map, _add_to_seed_queue(seed_queue, last_seed)}
+  defp _add_back_to_seed_queue({map, seed_queue, last_seed, dead_ends}) do
+    {map, _add_to_seed_queue(seed_queue, last_seed), dead_ends}
   end
 
-  defp _cull_surrounded_seeds({map, seed_queue}) do
-    {map, Enum.reject(seed_queue, fn(seed) -> _seed_surrounded(map, seed) end)}
+  defp _cull_surrounded_seeds({map, seed_queue, dead_ends}) do
+    dead_ends = Enum.reduce(seed_queue, dead_ends, &(_add_any_dead_end(map, &1, &2)))
+    {map, Enum.reject(seed_queue, fn(seed) -> _seed_surrounded(map, seed) end), dead_ends}
   end
 
-  defp _pop_from_seed_queue({map, [seed | seed_queue]}) do
-    {map, seed_queue, seed}
+  defp _pop_from_seed_queue({map, [seed | seed_queue], dead_ends}) do
+    {map, seed_queue, seed, dead_ends}
   end
 
-  defp _pop_from_seed_queue({map, []}) do
-    {map, [], []}
+  defp _pop_from_seed_queue({map, [], dead_ends}) do
+    {map, [], [], dead_ends}
   end
+
+  defp _add_any_dead_end(map, seed, dead_ends) do
+    if _dead_end(map, seed), do: [seed | dead_ends], else: dead_ends
+  end
+
+  defp _rand_range(min, max), do: :rand.uniform(max - min + 1) + min - 1
 end
