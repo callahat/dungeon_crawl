bf4e15b4d20e06c91b2c35387507e8b4bf516eec
callahat <tim.callahan25@yahoo.com>
2024-12-22 00:32:25 -0500

    libcluster config for prod

--- config/prod.exs
+++ config/prod.exs
@@ -55,3 +55,22 @@ config :logger, level: :info
 #
 #     config :dungeon_crawl, DungeonCrawl.Endpoint, server: true
 #
+
+config :libcluster,
+       topologies: [
+         epmd_example: [
+           # The selected clustering strategy. Required.
+           strategy: Cluster.Strategy.Epmd,
+           # Configuration for the provided strategy. Optional.
+           config: [hosts: [:"node@elixir-node1", :"node@elixir-node2"]],
+           # The function to use for connecting nodes. The node
+           # name will be appended to the argument list. Optional
+           connect: {:net_kernel, :connect_node, []},
+           # The function to use for disconnecting nodes. The node
+           # name will be appended to the argument list. Optional
+           disconnect: {:erlang, :disconnect_node, []},
+           # The function to use for listing nodes.
+           # This function must return a list of node names. Optional
+           list_nodes: {:erlang, :nodes, [:connected]},
+         ],
+       ]
