68bdec50f6af04472d1e4f09be52dbd4fb0fd0db
callahat <tim.callahan25@yahoo.com>
2026-05-13 00:05:18 -0400

    Fix preffered_cli_env deprecation warning

--- mix.exs
+++ mix.exs
@@ -10,14 +10,19 @@ defmodule DungeonCrawl.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: [
-       coveralls: :test,
-       "coveralls.detail": :test,
-       "coveralls.post": :test,
-       "coveralls.html": :test
-     ],
      aliases: aliases(),
      deps: deps()]
+  end
+
+  def cli do
+    [
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+    ]
   end
 
   # Configuration for the OTP application.
