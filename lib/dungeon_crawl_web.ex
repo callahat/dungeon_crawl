71f16d3faa1a6edd2c234bba3c6d76326b8c7e89
callahat <tim.callahan25@yahoo.com>
2026-05-20 22:43:29 -0400

    Update phoenix_html to address deprecation warnings being shown from there.

--- lib/dungeon_crawl_web.ex
+++ lib/dungeon_crawl_web.ex
@@ -85,7 +85,9 @@ defmodule DungeonCrawl.Web do
   defp view_helpers do
     quote do
       # Use all HTML functionality (forms, tags, etc)
-      use Phoenix.HTML
+      import Phoenix.HTML
+      import Phoenix.HTML.Form
+      use PhoenixHTMLHelpers
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
